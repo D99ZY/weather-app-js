575676f812d57ef1dd1327388e01c23747d28dfd
Damien <dtriegaardt@outlook.com>
2025-08-13 23:05:10 +1000

    Add weather API URLs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 function App() {
+  const WEATHER_KEY = import.meta.env.WEATHER_API_KEY;
+  const cityName = 'London';
+  // Gets lat and long coords
+  const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${WEATHER_KEY}`;
+  // Gets weather
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${WEATHER_KEY}`;
+
   return <>Hello</>;
 }
 
