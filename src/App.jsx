ef47528c5c318cb53bbeb6b540fbd46fd2d684d2
Damien <dtriegaardt@outlook.com>
2025-08-22 19:35:33 +1000

    Refactor Bottom section into component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import Search from './components/Search';
 import Top from './components/Top';
+import Bottom from './components/Bottom';
 
 function App() {
   // State variables
@@ -55,24 +56,7 @@ function App() {
       <Search ref={inputRef} handleSearch={handleSearch} />
       <div className="container">
         <Top weatherData={weatherData} />
-        <div className="bottom">
-          <div className="feels">
-            <p className="bold">
-              {weatherData ? Math.round(weatherData.main.feels_like - 273.15) : 26}°C
-            </p>
-            <p>Feels Like</p>
-          </div>
-          <div className="humidity">
-            <p className="bold">{weatherData ? Math.round(weatherData.main.humidity) : 50}%</p>
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            <p className="bold">
-              {weatherData ? Math.round(weatherData.wind.speed * 2.237) : 12}mph
-            </p>
-            <p>Wind Speed</p>
-          </div>
-        </div>
+        <Bottom weatherData={weatherData} />
       </div>
     </div>
   );
---
+++ src/components/Bottom.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+
+function Bottom(props) {
+  const weatherData = props;
+
+  return (
+    <div className="bottom">
+      <div className="feels">
+        <p className="bold">
+          {weatherData ? Math.round(weatherData.main.feels_like - 273.15) : 26}°C
+        </p>
+        <p>Feels Like</p>
+      </div>
+      <div className="humidity">
+        <p className="bold">{weatherData ? Math.round(weatherData.main.humidity) : 50}%</p>
+        <p>Humidity</p>
+      </div>
+      <div className="wind">
+        <p className="bold">{weatherData ? Math.round(weatherData.wind.speed * 2.237) : 12}mph</p>
+        <p>Wind Speed</p>
+      </div>
+    </div>
+  );
+}
+
+Bottom.propTypes = {
+  weatherData: PropTypes.shape({
+    main: PropTypes.shape({
+      feels_like: PropTypes.number.isRequired,
+      humidity: PropTypes.number.isRequired,
+    }),
+    wind: PropTypes.shape({
+      speed: PropTypes.number.isRequired,
+    }),
+  }).isRequired,
+};
+
+export default Bottom;
